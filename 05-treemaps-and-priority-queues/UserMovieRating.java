a64ce4953731b99158d3aa1f103ded91f38bc012
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-02-24 08:22:30 +0100

    Add UserMovieRating class documentation

--- 05-treemaps-and-priority-queues/UserMovieRating.java
+++ 05-treemaps-and-priority-queues/UserMovieRating.java
@@ -1,27 +1,47 @@
-/*
- * SD2x Homework #5
+/**
+ * SD2x Homework #5 - Movie Rating
  * This class represents a single rating for a movie.
  * Please do not change this code! Your solution will be evaluated using this version of the class.
+ *
+ * @author Solange U. Gasengayire
  */
-
 public class UserMovieRating implements Comparable<UserMovieRating> {
 
 	protected String movie;
 	protected int userRating;
-	
+
+	/**
+	 * Constructor
+	 * @param movie the title of the movie
+	 * @param userRating the user rating of the movie
+	 */
 	public UserMovieRating(String movie, int userRating) {
 		this.movie = movie;
 		this.userRating = userRating;
 	}
-	
+
+	/**
+	 * Return this movie's title
+	 * @return the title
+	 */
 	public String getMovie() {
 		return movie;
 	}
-	
+
+	/**
+	 * Return this movie's rating
+	 * @return the rating
+	 */
 	public int getUserRating() {
 		return userRating;
 	}
-	
+
+	/**
+	 * Determine equality between two movies
+	 * @param obj the other movie
+	 * @return true of this and the other movie are equal
+	 * 		   false otherwise
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -41,6 +61,11 @@ public class UserMovieRating implements Comparable<UserMovieRating> {
 		return true;
 	}
 
+	/**
+	 * Compare this movie to another
+	 * @param other the other movie to compare to
+	 * @return the result of the comparison
+	 */
 	@Override
 	public int compareTo(UserMovieRating other) {
 		return this.userRating - other.userRating;

bfae05ede9cc857bdd3b9a489b11d070518f34d3
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-02-24 08:21:03 +0100

    Add MovieRatingsParser class documentation

--- 05-treemaps-and-priority-queues/MovieRatingsParser.java
+++ 05-treemaps-and-priority-queues/MovieRatingsParser.java
@@ -1,20 +1,26 @@
-/*
- * SD2x Homework #5
- * Implement the method below according to the specification in the assignment description.
- * Please be sure not to change the method signature!
- */
-
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.TreeMap;
 
+/**
+ * SD2x Homework #5 - Movie Rating Parser
+ * Implement the method below according to the specification in the assignment description.
+ * Please be sure not to change the method signature!
+ *
+ * @author Solange U. Gasengayire
+ */
 public class MovieRatingsParser {
 
+	/**
+	 * Parse a list of movie ratings into a TreeMap
+	 * @param allUsersRatings input list of movie ratings
+	 * @return the parsed TreeMap
+	 */
 	public static TreeMap<String, PriorityQueue<Integer>> parseMovieRatings(List<UserMovieRating> allUsersRatings) {
 		
 		/* IMPLEMENT THIS METHOD! */
 		
-		return null; // this line is here only so this code will compile if you don't modify it
+		return null;
 	}
 
 }

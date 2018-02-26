4c0182c83fb6794271a7c586d1fa488e29200d07
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-02-26 14:08:08 +0100

    Add Edge class and methods documentation

--- 06-graphs/Edge.java
+++ 06-graphs/Edge.java
@@ -1,15 +1,26 @@
-
-
-
+/**
+ * A class that represents an edge in a graph
+ */
 public class Edge {
 	private final Node source;
 	private final Node destination;
-	
+
+	/**
+	 * Constructor
+	 * @param source the source node
+	 * @param destination the destination node
+	 */
 	public Edge(Node source, Node destination) {
 		this.source = source;
 		this.destination = destination;
 	}
-	
+
+	/**
+	 * Is this edge equal to the other?
+	 * @param obj the other edge to compare to
+	 * @return true if the two edges are equal
+	 * 		   false otherwise
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (!(obj instanceof Edge)) {
@@ -22,16 +33,28 @@ public class Edge {
 		return (otherSource.equals(source)
 			 && otherDest.equals(destination));
 	}
-	
+
+	/**
+	 * Return this edge integer representation
+	 * @return this edge as an int
+	 */
 	@Override
 	public int hashCode() {
 		return source.hashCode() + destination.hashCode();
 	}
-	
+
+	/**
+	 * Return this edge's source node
+	 * @return source node
+	 */
 	public Node getSource() {
 		return source;
 	}
-	
+
+	/**
+	 * Return this edge's destination node
+	 * @return destination node
+	 */
 	public Node getDestination() {
 		return destination;
 	}

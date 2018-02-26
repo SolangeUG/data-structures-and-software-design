d9515421e19fc60883f9447636d5156ed692b825
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-02-26 14:05:22 +0100

    Add Node class and methods documentation

--- 06-graphs/Node.java
+++ 06-graphs/Node.java
@@ -1,17 +1,29 @@
-
-
-
+/**
+ * A class that represents a node in a graph
+ */
 public class Node {
 	protected String element;
-	
+
+	/**
+	 * Constructor
+	 * @param element the value of this node
+	 */
 	public Node(String element) {
 		this.element = element;
 	}
-	
+
+	/**
+	 * Return the value of this node
+	 * @return element
+	 */
 	public String getElement() {
 		return element;
 	}
-	
+
+	/**
+	 * Return the string representation of this node
+	 * @return this node as a string
+	 */
 	public String toString() {
 		return element;
 	}

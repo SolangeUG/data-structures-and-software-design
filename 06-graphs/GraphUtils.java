2daa42c2f21d8eddcb3a4e153091e4f0f5a60b6d
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-02-26 14:45:47 +0100

    Add GraphUtils class and methods documentation

--- 06-graphs/GraphUtils.java
+++ 06-graphs/GraphUtils.java
@@ -1,37 +1,53 @@
-
-
 import java.util.List;
 import java.util.Set;
 
-/*
- * SD2x Homework #6
+/**
+ * SD2x Homework #6 - Methods used to explore graphs.
  * Implement the methods below according to the specification in the assignment description.
  * Please be sure not to change the signature of any of the methods!
+ *
+ * @author Solange U. Gasengayire
  */
 
 public class GraphUtils {
 
+	/**
+	 * Determine the shortest distance from an element to another in a graph
+	 * @param graph the input graph to explore
+	 * @param src the source element
+	 * @param dest the destination element
+	 * @return the shortest distance between the two elements
+	 */
 	public static int minDistance(Graph graph, String src, String dest) {
 
-		/* IMPLEMENT THIS METHOD! */
 		
-		return -2; // this line is here only so this code will compile if you don't modify it
+		return -2;
 	}
-	
 
+	/**
+	 * Return a set of elements within the specified distance of a source node
+	 * @param graph the input graph to explore
+	 * @param src the source element
+	 * @param distance the provided distance
+	 * @return the set of all elements within distance of the source element
+	 */
 	public static Set<String> nodesWithinDistance(Graph graph, String src, int distance) {
 
-		/* IMPLEMENT THIS METHOD! */
 		
-		return null; // this line is here only so this code will compile if you don't modify it
+		return null;
 	}
 
-
+	/**
+	 * Determine whether the provided list of values make up a Hamiltonian path in a graph
+	 * @param g the input graph to explore
+	 * @param values the list of elements
+	 * @return true if the values constitute a Hamiltonian path in the graph
+	 * 		   false otherwise
+	 */
 	public static boolean isHamiltonianPath(Graph g, List<String> values) {
 
-		/* IMPLEMENT THIS METHOD! */
 		
-		return true; // this line is here only so this code will compile if you don't modify it
+		return true;
 	}
 	
 }

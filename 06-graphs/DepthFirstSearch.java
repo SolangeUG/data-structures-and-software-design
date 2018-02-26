95e7fca19570cec395313907b4db1dea85226b62
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-02-26 14:18:39 +0100

    Add DepthFirstSearch class and methods documentation

--- 06-graphs/DepthFirstSearch.java
+++ 06-graphs/DepthFirstSearch.java
@@ -1,10 +1,8 @@
-
-
 import java.util.HashSet;
 import java.util.Set;
 
-/*
- * SD2x Homework #6
+/**
+ * SD2x Homework #6 - Depth-First Search
  * This is an implementation of Depth First Search (DFS) on a graph.
  * You may modify and submit this code if you'd like.
  */
@@ -12,12 +10,23 @@ import java.util.Set;
 public class DepthFirstSearch {
 	protected Set<Node> marked;
 	protected Graph graph;
-	
+
+	/**
+	 * Constructor
+	 * @param graphToSearch the input graph
+	 */
 	public DepthFirstSearch(Graph graphToSearch) {
 		marked = new HashSet<Node>();
 		graph = graphToSearch;
 	}
-	
+
+	/**
+	 * Determine whether a path exists between a given node and an element in the graph
+	 * @param start the source node
+	 * @param elementToFind the element to find
+	 * @return true if such a path exists
+	 * 		   false otherwise
+	 */
 	public boolean dfs(Node start, String elementToFind) {
 		if (!graph.containsNode(start)) {
 			return false;

e112e1678af26eff5e761142a4a8ffdec42a8d35
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-02-26 14:12:55 +0100

    Add DirectedGraph class and methods documentation

--- 06-graphs/DirectedGraph.java
+++ 06-graphs/DirectedGraph.java
@@ -1,12 +1,22 @@
-
-
-
+/**
+ * A concrete class that represents a directed graph
+ */
 public class DirectedGraph extends Graph {
-	
+
+	/**
+	 * Constructor
+	 */
 	public DirectedGraph() {
 		super();
 	}
-	
+
+	/**
+	 * Add an edge to the graph
+	 * @param source the source node
+	 * @param destination the destination node
+	 * @return true if the operation succeeds
+	 * 		   false otherwise
+	 */
 	@Override
 	public boolean addEdge(Node source, Node destination) {
 		addNode(source); // only adds if node not already in graph.
@@ -18,6 +28,13 @@ public class DirectedGraph extends Graph {
 		return addEdgeSuccess;	
 	}
 
+	/**
+	 * Remove an edge from the graph
+	 * @param source the source node
+	 * @param destination the destination node
+	 * @return true if the operation succeeds
+	 * 		   false otherwise
+	 */
 	@Override
 	public boolean removeEdge(Node source, Node destination) {
 		if (!containsNode(source) || !containsNode(destination)) {

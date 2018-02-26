5cf0169f920c0650dcaf3d2668f609a86beaffd9
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-02-26 14:16:22 +0100

    Add BreadthFirstSearch class and methods documentation

--- 06-graphs/BreadthFirstSearch.java
+++ 06-graphs/BreadthFirstSearch.java
@@ -1,12 +1,10 @@
-
-
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Set;
 
-/*
- * SD2x Homework #6
+/**
+ * SD2x Homework #6 - Breadth-First Search
  * This is an implementation of Breadth First Search (BFS) on a graph.
  * You may modify and submit this code if you'd like.
  */
@@ -15,13 +13,21 @@ public class BreadthFirstSearch {
 	protected Set<Node> marked;
 	protected Graph graph;
 
+	/**
+	 * Constructor
+	 * @param graphToSearch the input graph
+	 */
 	public BreadthFirstSearch(Graph graphToSearch) {
 		marked = new HashSet<Node>();
 		graph = graphToSearch;
 	}
-	
+
 	/**
-	 * This method was discussed in the lesson
+	 * Determine whether there is a path from a node to an element in a graph
+	 * @param start the starting node
+	 * @param elementToFind the element to find
+	 * @return true if a path exists from the source node to the element
+	 * 		   false otherwise
 	 */
 	public boolean bfs(Node start, String elementToFind) {
 		if (!graph.containsNode(start)) {

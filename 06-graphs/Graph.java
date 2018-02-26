b989f790594a1a19cfab404d45cae504a7da48cb
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-02-26 14:03:13 +0100

    Add Graph class and methods documentation

--- 06-graphs/Graph.java
+++ 06-graphs/Graph.java
@@ -1,12 +1,12 @@
-
-
-
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * A class that represents a graph as an adjacency set
+ */
 public abstract class Graph {
 	protected Map<Node, Set<Edge>> adjacencySets;
 	protected int numNodes;
@@ -19,14 +19,22 @@ public abstract class Graph {
 	public abstract boolean addEdge(Node node1, Node node2);
 	public abstract boolean removeEdge(Node node1, Node node2);
 
+	/**
+	 * Constructor
+	 */
 	public Graph() {
 		adjacencySets = new HashMap<Node, Set<Edge>>();
 		elementToNode = new HashMap<String, Node>();
 		numNodes = 0;
 		numEdges = 0;
 	}
-	
-	
+
+	/**
+	 * Add a node to the graph
+	 * @param newNode the node to be added
+	 * @return true if the operation succeeds
+	 * 		   false otherwise
+	 */
 	public boolean addNode(Node newNode) {
 		if (newNode == null || containsNode(newNode)) {
 			return false;
@@ -37,7 +45,12 @@ public abstract class Graph {
 		numNodes++;
 		return true;
 	}
-	
+
+	/**
+	 * Return the given node's neighbors in the graph
+	 * @param node the input node
+	 * @return the set of the input node's neighbors
+	 */
 	public Set<Node> getNodeNeighbors(Node node) {
 		if (!containsNode(node)) {
 			return null;
@@ -50,8 +63,14 @@ public abstract class Graph {
 		}
 		return nodeNeighbors;
 	}
-	
-		
+
+	/**
+	 * Add an edge between two nodes
+ 	 * @param source the source node
+	 * @param destination the destination node
+	 * @return true if the operation succeeds
+	 * 		   false otherwise
+	 */
 	protected boolean addEdgeFromTo(Node source,
 			Node destination) {
 		Edge newEdge = new Edge(source, destination);
@@ -62,7 +81,14 @@ public abstract class Graph {
 		}
 		return false;
 	}
-	
+
+	/**
+	 * Delete an edge connecting two nodes
+	 * @param source the source node
+	 * @param destination the destination node
+	 * @return true if the operation succeeds
+	 * 		   false otherwise
+	 */
 	protected boolean removeEdgeFromTo(Node source,
 			Node destination) {
 		Edge toRemove = new Edge(source, destination);
@@ -73,16 +99,28 @@ public abstract class Graph {
 		}
 		return false;
 	}
-	
-	
+
+	/**
+	 * Return the number of nodes in the graph
+	 * @return node count
+	 */
 	public int getNumNodes() {
 		return numNodes;
 	}
-	
+
+	/**
+	 * Return the number of edges in the graph
+	 * @return edge count
+	 */
 	public int getNumEdges() {
 		return numEdges;
 	}
-	
+
+	/**
+	 * Return the starting node
+	 * @return the staring node if the graph is not empty
+	 * 		   null otherwise
+	 */
 	public Node getStartingNode() {
 		Iterator<Node> iter = adjacencySets.keySet().iterator();
 		if (iter.hasNext()) {
@@ -90,22 +128,42 @@ public abstract class Graph {
 		}
 		return null;
 	}
-	
+
+	/**
+	 * Return a set of all the nodes of the graph
+	 * @return all the nodes in the graph
+	 */
 	public Set<Node> getAllNodes() {
 		return adjacencySets.keySet();
 	}
-	
+
+	/**
+	 * Return a set of edges from and to the given node
+	 * @param node the input node
+	 * @return all the edges adjacent to the given node
+	 */
 	public Set<Edge> getNodeEdges(Node node) {
 		if (!containsNode(node)) {
 			return null;
 		}
 		return adjacencySets.get(node);
 	}
-	
+
+	/**
+	 * Does the graph contain a given node?
+	 * @param node the input node
+	 * @return true if the node is in the graph
+	 * 		   false otherwise
+	 */
 	public boolean containsNode(Node node) {
 		return adjacencySets.containsKey(node);
 	}
-	
+
+	/**
+	 * Return the node that corresponds to a given element
+	 * @param element the input string element
+	 * @return its corresponding node
+	 */
 	public Node getNode(String element) {
 		
 		if (!elementToNode.containsKey(element)) {
@@ -116,7 +174,13 @@ public abstract class Graph {
     	
 		return elementToNode.get(element);
 	}
-	
+
+	/**
+	 * Determine whether the graph contains a given string element
+	 * @param element the input value
+	 * @return true if the element is found in the graph
+	 * 		   false otherwise
+	 */
 	public boolean containsElement(String element) {
 		return elementToNode.containsKey(element);
 	}

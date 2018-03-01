e52ce11df34f849637218debeb88720d71a5489f
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-03-01 20:02:09 +0100

    Add Java class for the Door UML specification

---
+++ 07-UML/Door.java
@@ -0,0 +1,28 @@
+/**
+ * This class represents a room in a maze game
+ * @author Solange U. Gasengayire
+ */
+public class Door extends MapSite {
+
+    // exceptionally, for the purpose of this project,
+    // attributes are declared public
+    public boolean isOpen;
+    public Room room1;
+    public Room room2;
+
+    /**
+     * No-argument constructor
+     */
+    public Door() {
+        super();
+        // voluntarily not implemented
+    }
+
+    /**
+     * Enter method
+     */
+    public void enter() {
+        // voluntarily not implemented
+    }
+
+}

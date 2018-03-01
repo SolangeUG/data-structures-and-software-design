e2b7f6fea810687555d7d8243ea1873dcc8e8eba
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-03-01 19:59:21 +0100

    Add Java class for the MapSite UML specification

---
+++ 07-UML/MapSite.java
@@ -0,0 +1,22 @@
+/**
+ * This class represents a map site
+ * @author Solange U. Gasengayire
+ */
+public abstract class MapSite {
+
+    /**
+     * No-argument constructor
+     */
+    public MapSite() {
+        super();
+        // voluntarily not implemented
+    }
+
+    /**
+     * Enter the map site
+     */
+    public void enter() {
+        // voluntarily no nothing
+    }
+
+}

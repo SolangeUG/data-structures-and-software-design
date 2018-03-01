01510757a6f82b0345de999b21d4a24bd0814902
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-03-01 20:01:42 +0100

    Add Java class for the Maze UML specification

---
+++ 07-UML/Maze.java
@@ -0,0 +1,26 @@
+/**
+ * This class represents a maze
+ * @author Solange U. Gasengayire
+ */
+public class Maze {
+
+    // exceptionally, for the purpose of this project
+    // attributes are declared public
+    public Room[] rooms;
+
+    /**
+     * No-argument constructor
+     */
+    public Maze() {
+        super();
+        // voluntarily not implemented
+    }
+
+    /**
+     * Add a room to this maze
+     * @param room the room to be added
+     */
+    public void addRoom(Room room) {
+        // voluntarily not implemented
+    }
+}

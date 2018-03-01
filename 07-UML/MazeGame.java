e25b4b8111a770483918e8ace8051d4a960ae3ee
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-03-01 20:00:12 +0100

    Add Java class for the MazeGame UML specification

---
+++ 07-UML/MazeGame.java
@@ -0,0 +1,31 @@
+/**
+ * This class represents a maze game
+ * @author Solange U. Gasengayire
+ */
+public class MazeGame {
+
+    /**
+     * No-argument constructor
+     */
+    public MazeGame() {
+        super();
+        // voluntarily not implemented
+    }
+
+    /**
+     * Create a maze for the game
+     * @return a new maze
+     */
+    public Maze createMaze() {
+        // voluntarily not implemented
+        return null;
+    }
+
+    /**
+     * Load a maze
+     */
+    public void loadMaze() {
+        // voluntarily not implemented
+    }
+
+}

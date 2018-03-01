07b98ce81287028e6afb2fc7975975a2ab63c094
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-03-01 20:00:42 +0100

    Add Java class for the RandomizedMazeGame UML specification

---
+++ 07-UML/RandomizedMazeGame.java
@@ -0,0 +1,25 @@
+/**
+ * This class represents a randomized maze game
+ * @author Solange U. Gasengayire
+ */
+public class RandomizedMazeGame extends MazeGame {
+
+    /**
+     * No-argument constructor
+     */
+    public RandomizedMazeGame() {
+        super();
+        // voluntarily not implemented
+    }
+
+    /**
+     * Create a randomized-size maze
+     * @param random the random size
+     * @return a new maze
+     */
+    public Maze randomize(int random) {
+        // voluntarily not implemented
+        return null;
+    }
+
+}

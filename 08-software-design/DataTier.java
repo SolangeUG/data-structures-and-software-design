60c1345c88a7787fcc52172f932b1c0baae09e91
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-03-02 19:00:13 +0100

    Implement getAllBooks method in the DataTier class

--- 08-software-design/DataTier.java
+++ 08-software-design/DataTier.java
@@ -1,16 +1,51 @@
-/*
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
  * SD2x Homework #8
  * This class represents the Data Tier in the three-tier architecture.
  * Implement the appropriate methods for this tier below.
  */
-
 public class DataTier {
-	
-	private String fileName; // the name of the file to read
-	
+
+	// name of the file to be read
+	private String fileName;
+
+	/**
+	 * Constructor
+	 * @param inputSource name of the input file
+	 */
 	public DataTier(String inputSource) {
 		fileName = inputSource;
 	}
-	
+
+	/**
+	 * Return a list of Book objects
+	 * @return book objects
+	 */
+	public List<Book> getAllBooks() {
+		List<Book> books = new LinkedList<>();
+
+		// read data from the input file
+		if (fileName != null && !fileName.isEmpty()) {
+			try (InputStream fileStream = new FileInputStream(fileName)) {
+				BufferedReader reader =
+						new BufferedReader(new InputStreamReader(fileStream));
+				String line = null;
+				while ((line = reader.readLine()) != null)  {
+					// each row of the input file is formatted as follows:
+					// [title] tab [author] tab [year]
+					String[] data = line.split("\\t");
+					Book book = new Book(data[0], data[1], Integer.valueOf(data[2]));
+					books.add(book);
+				}
+
+			} catch (IOException exception) {
+				System.out.println("Error while processing the data input file: " + exception.getMessage());
+			}
+		}
+		return books;
+	}
 
 }

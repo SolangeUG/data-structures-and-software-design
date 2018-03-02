380c3854afe2952869b369e7dea8ae6d1d2e7ed7
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-03-02 18:44:24 +0100

    Add Main class documentation

--- 08-software-design/Main.java
+++ 08-software-design/Main.java
@@ -1,11 +1,14 @@
-/*
- * SD2x Homework #8
+/**
+ * SD2x Homework #8 - Application Entry Point
  * This class creates the classes in the three tiers and links them together.
  * You should not modify this code. You do not need to submit it.
  */
-
 public class Main {
-	
+
+	/**
+	 * Application Main Method
+	 * @param args program arguments
+	 */
 	public static void main(String[] args) {
 		
 		DataTier dt = new DataTier(args[0]);

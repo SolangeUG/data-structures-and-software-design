92d24b83ba0b2fde42e05082df69cc82bb6a74fa
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-03-02 18:42:38 +0100

    Add Book class and method documentation

--- 08-software-design/Book.java
+++ 08-software-design/Book.java
@@ -1,11 +1,14 @@
-/*
+/**
  * SD2x Homework #8
  * This class represents a single book.
  * Please do not change this code! Your solution will be evaluated using this version of the class.
  */
-
 public class Book implements Comparable<Book> {
 
+	/**
+	 * String representation of this book
+	 * @return this book as a string
+	 */
 	@Override
 	public String toString() {
 		return "Book [title=" + title + ", author=" + author
@@ -15,25 +18,49 @@ public class Book implements Comparable<Book> {
 	protected String title;
 	protected String author;
 	protected int publicationYear;
-	
+
+	/**
+	 * Constructor
+	 * @param title book title
+	 * @param author book author
+	 * @param year year of publication
+	 */
 	public Book(String title, String author, int year) {
 		this.title = title;
 		this.author = author;
 		this.publicationYear = year;
 	}
-	
+
+	/**
+	 * Return this book's title
+	 * @return title
+	 */
 	public String getTitle() {
 		return title;
 	}
-	
+
+	/**
+	 * Return this book's author
+	 * @return author
+	 */
 	public String getAuthor() {
 		return author;
 	}
-	
+
+	/**
+	 * Return this book's publication year
+	 * @return publication year
+	 */
 	public int getPublicationYear() {
 		return publicationYear;
 	}
 
+	/**
+	 * Determine equality between two books
+	 * @param obj the other object
+	 * @return true if this book is equal to the other
+	 * 		   false otherwise
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -58,11 +85,20 @@ public class Book implements Comparable<Book> {
 		return true;
 	}
 
+	/**
+	 * Compare this book to the other
+	 * @param other the other book to compare to
+	 * @return the result of the comparison
+	 */
 	@Override
 	public int compareTo(Book other) {
 		return this.publicationYear - other.publicationYear;
 	}
-	
+
+	/**
+	 * Integer representation of this book
+	 * @return this book as an int
+	 */
 	@Override
 	public int hashCode() {
 		return this.author.hashCode();

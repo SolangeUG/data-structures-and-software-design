2653ad4bb1b3b65a33a15c3773d289c18b058648
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-03-07 09:59:02 +0100

    Use of defensive programming to avoid any NullPointerExceptions in findClassMates

--- 10-reliability/FriendFinder.java
+++ 10-reliability/FriendFinder.java
@@ -30,45 +30,79 @@ public class FriendFinder {
 	 * @return friend suggestions
 	 */
 	public Set<String> findClassmates(Student theStudent) {
-		
-		String name = theStudent.getName();
-		
-		// find the classes that this student is taking
-		List<String> myClasses = classesDataSource.getClasses(name);
-		
+
+		// The input value shouldn't be null
+		if (theStudent == null || theStudent.getName() == null) {
+			throw new IllegalArgumentException("Input value theStudent is null or inappropriate");
+		}
+
+		// The internal property classesDataSource shouldn't be null
+		if (classesDataSource == null || studentsDataSource == null) {
+			throw new IllegalStateException("Interal properties classesDataSource and studentsDataSource shouldn't be null");
+		}
+
 		Set<String> classmates = new HashSet<>();
-		
-		// use the classes to find the names of the students
-		for (String myClass : myClasses) {
-			// list all the students in the class
-			List<Student> students = studentsDataSource.getStudents(myClass);
-			
-			for (Student student : students) {
-				
-				// find the other classes that they're taking
-				List<String> theirClasses = classesDataSource.getClasses(student.getName());
-			
-				// see if all of the classes that they're taking are the same as the ones this student is taking
-				boolean same = true;
-				for (String c : myClasses) {
-					
-					if (theirClasses.contains(c) == false) {
-						same = false;
-						break;
+		String name = theStudent.getName();
+
+		if (name != null) {
+
+			// find the classes that this student is taking
+			List<String> myClasses = classesDataSource.getClasses(name);
+
+			// Make sure that the list of classes returned previously isn't null
+			if (myClasses != null) {
+
+				// use the classes to find the names of the students
+				for (String myClass : myClasses) {
+
+					// Make sure the list of classes does not contain any null values
+					if (myClass != null) {
+
+						// list all the students in the class
+						List<Student> students = studentsDataSource.getStudents(myClass);
+
+						// Make sure that the list of students returned previously isn't null or empty
+						if (students != null && !students.isEmpty()) {
+
+							for (Student student : students) {
+
+								// Make sure the list of students does not contain any null values
+								if (student != null && student.getName() != null) {
+
+									// find the other classes that they're taking
+									List<String> theirClasses = classesDataSource.getClasses(student.getName());
+
+									// Make sure that theirClasses is not null
+									if (theirClasses != null && ! theirClasses.isEmpty()) {
+
+										// see if all of the classes that they're taking are the same as the ones this student is taking
+										boolean same = true;
+										for (String c : myClasses) {
+											// Make sure the list of classes does not contain any null values
+											if (c != null) {
+												if (! theirClasses.contains(c)) {
+													same = false;
+													break;
+												}
+											}
+										}
+										if (same) {
+											if (! student.getName().equals(name) && !classmates.contains(student.getName()))
+												classmates.add(student.getName());
+										}
+									}
+								}
+							}
+						}
 					}
 				}
-				if (same) {
-					if (student.getName().equals(name) == false && classmates.contains(student.getName()) == false) 
-						classmates.add(student.getName());
-				}
 			}
+		}
 
-		}
-				
-		if (classmates.isEmpty()) { 
+		if (classmates.isEmpty())
 			return null;
-		}
-		else return classmates;
+		else
+			return classmates;
 	}
 	
 

30c1598c5d8557daaf99f564605f7b428e691a36
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-03-07 09:22:00 +0100

    Add class and method documentation

--- 10-reliability/FriendFinder.java
+++ 10-reliability/FriendFinder.java
@@ -1,25 +1,34 @@
-
 import java.util.List;
 import java.util.HashSet;
 import java.util.Set;
 
-/*
- * SD2x Homework #10
+/**
+ * SD2x Homework #10 - Reliability
  * Modify the method below so that it uses defensive programming.
  * Please be sure not to change the method signature!
+ *
+ * @author Solange U. Gasengayire
  */
-
 public class FriendFinder {
 	
 	protected ClassesDataSource classesDataSource;
 	protected StudentsDataSource studentsDataSource;
-	
+
+	/**
+	 * Constructor
+	 * @param cds class data source
+	 * @param sds student data source
+	 */
 	public FriendFinder(ClassesDataSource cds, StudentsDataSource sds) {
 		classesDataSource = cds;
 		studentsDataSource = sds;
 	}
-	
-	
+
+	/**
+	 * Return friend suggestions based on common classes
+	 * @param theStudent the student to suggest friends to
+	 * @return friend suggestions
+	 */
 	public Set<String> findClassmates(Student theStudent) {
 		
 		String name = theStudent.getName();
@@ -27,7 +36,7 @@ public class FriendFinder {
 		// find the classes that this student is taking
 		List<String> myClasses = classesDataSource.getClasses(name);
 		
-		Set<String> classmates = new HashSet<String>();
+		Set<String> classmates = new HashSet<>();
 		
 		// use the classes to find the names of the students
 		for (String myClass : myClasses) {
